5ba66c7260daf35ed3dea85388e693d491b95bab
glourencosilva <giliardlourenco@gmail.com>
2022-05-11 23:11:06 -0300

    Create model class PlaceLocation.

--- lib/model/places.dart
+++ lib/model/places.dart
@@ -1,17 +1,19 @@
+import 'dart:io';
+
 import 'package:equatable/equatable.dart';
 
 class Places extends Equatable {
   final String id;
   final String title;
   final Object location;
-  final Object images;
+  final File images;
 
-  const Places(
-    this.id,
-    this.title,
-    this.location,
-    this.images,
-  );
+  const Places({
+    required this.id,
+    required this.title,
+    required this.location,
+    required this.images,
+  });
 
   @override
   List<Object?> get props => [id, title, location, images];
