8ca82b8e642972f9f1716049df8a60c3668574b9
glourencosilva <giliardlourenco@gmail.com>
2022-05-11 23:17:08 -0300

    Add propertie address PlaceLocation.

--- lib/model/place_location.dart
+++ lib/model/place_location.dart
@@ -3,12 +3,11 @@ import 'package:equatable/equatable.dart';
 class PlaceLocation extends Equatable {
   final double latitude;
   final double longitude;
+  final String address;
 
-  const PlaceLocation({
-    required this.latitude,
-    required this.longitude,
-  });
+  const PlaceLocation(
+      {required this.latitude, required this.longitude, required this.address});
 
   @override
-  List<Object?> get props => [latitude, longitude];
+  List<Object?> get props => [latitude, longitude, address];
 }
